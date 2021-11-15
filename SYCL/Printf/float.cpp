b376aa5bc432a0935a1af3f0cab4e056ab7ac943
Alexey Sachkov <alexey.sachkov@intel.com>
2021-11-15 20:22:19 +0300

    Add more tests

---
+++ SYCL/Printf/float.cpp
@@ -0,0 +1,78 @@
+// This test is written with an aim to check that experimental::printf behaves
+// in the same way as printf from C99/C11
+//
+// The test is written using conversion specifiers table from cppreference [1]
+// [1]: https://en.cppreference.com/w/cpp/io/c/fprintf
+//
+// RUN: %clangxx -fsycl %s -o %t.out
+// RUN: %CPU_RUN_PLACEHOLDER %t.out %CPU_CHECK_PLACEHOLDER
+// RUN: %GPU_RUN_PLACEHOLDER %t.out %GPU_CHECK_PLACEHOLDER
+// RUN: %ACC_RUN_PLACEHOLDER %t.out %ACC_CHECK_PLACEHOLDER
+//
+// CHECK: float %e=3.140000e+00, %E=3.140000E+00
+// CHECK: double %e=-6.813800e+00, %E=-6.813800E+00
+// CHECK: mixed %e=3.140000e+00, %E=-6.813800E+00
+// CHECK: float %a=0x1.91eb86p+1, %A=0X1.91EB86P+1
+// CHECK: double %a=-0x1.b4154d8cccccdp+2, %A=-0X1.B4154D8CCCCCDP+2
+// CHECK: float %g=3.14, %G=3.14
+// CHECK: double %g=-6.8138, %G=-6.8138
+
+#include <CL/sycl.hpp>
+
+#include "helper.hpp"
+
+using namespace sycl;
+
+void do_float_test() {
+  {
+    // %e, %E floating-point, decimal exponent notation
+    FORMAT_STRING(fmt1) = "float %%e=%e, %%E=%E\n";
+    FORMAT_STRING(fmt2) = "double %%e=%e, %%E=%E\n";
+    FORMAT_STRING(fmt3) = "mixed %%e=%e, %%E=%E\n";
+
+    float f = 3.14;
+    double d = -f * 2.17;
+    ext::oneapi::experimental::printf(fmt1, f, f);
+    ext::oneapi::experimental::printf(fmt2, d, d);
+    ext::oneapi::experimental::printf(fmt3, f, d);
+  }
+
+  {
+    // %a, %A floating-point, hexadecimal exponent notation
+    FORMAT_STRING(fmt1) = "float %%a=%a, %%A=%A\n";
+    FORMAT_STRING(fmt2) = "double %%a=%a, %%A=%A\n";
+    FORMAT_STRING(fmt3) = "mixed %%a=%a, %%A=%A\n";
+
+    float f = 3.14;
+    double d = -f * 2.17;
+    ext::oneapi::experimental::printf(fmt1, f, f);
+    ext::oneapi::experimental::printf(fmt2, d, d);
+    ext::oneapi::experimental::printf(fmt3, f, d);
+  }
+
+  {
+    // %g, %G floating-point
+    FORMAT_STRING(fmt1) = "float %%g=%g, %%G=%G\n";
+    FORMAT_STRING(fmt2) = "double %%g=%g, %%G=%G\n";
+    FORMAT_STRING(fmt3) = "mixed %%g=%g, %%G=%G\n";
+
+    float f = 3.14;
+    double d = -f * 2.17;
+    ext::oneapi::experimental::printf(fmt1, f, f);
+    ext::oneapi::experimental::printf(fmt2, d, d);
+    ext::oneapi::experimental::printf(fmt3, f, d);
+  }
+}
+
+class FloatTest;
+
+int main() {
+  queue q;
+
+  q.submit([](handler &cgh) {
+    cgh.single_task<FloatTest>([]() { do_float_test(); });
+  });
+  q.wait();
+
+  return 0;
+}
