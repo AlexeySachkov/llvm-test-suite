8cfb6f4c98e7f7d71f6d7f2da3d8bb37e0496759
Alexey Sachkov <alexey.sachkov@intel.com>
2022-11-08 08:27:13 -0500

    Apply review comments

--- SYCL/Regression/set-arg-local-accessor.cpp
+++ SYCL/Regression/set-arg-local-accessor.cpp
@@ -1,6 +1,6 @@
-// REQUIRES: opencl
+// REQUIRES: opencl, opencl_icd
 //
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -lOpenCL -o %t.out
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s %opencl_lib -o %t.out
 // RUN: env SYCL_DEVICE_FILTER=opencl %CPU_RUN_PLACEHOLDER %t.out
 // RUN: env SYCL_DEVICE_FILTER=opencl %GPU_RUN_PLACEHOLDER %t.out
 // RUN: env SYCL_DEVICE_FILTER=opencl %ACC_RUN_PLACEHOLDER %t.out
@@ -11,10 +11,10 @@ using namespace sycl;
 
 const char *clSource = R"(
     kernel void test(global int *a, local float *b, int n) {
-        if (get_local_id(0) != 0)
-            return;
-        for (int i = 0; i < n; i++)
-            b[i] = i;
+        if (get_local_id(0) != 0) {
+          for (int i = 0; i < n; i++)
+              b[i] = i;
+        }
         barrier(CLK_LOCAL_MEM_FENCE);
 
         bool ok = true;
@@ -25,22 +25,18 @@ const char *clSource = R"(
     }
 )";
 
-void handle(cl_int status) {
-  if (status != CL_SUCCESS)
-    throw std::runtime_error("OpenCL error occurred");
-}
-
 kernel getKernel(const queue &Q) {
-  cl_int status;
+  cl_int status = CL_SUCCESS;
 
   auto device_cl = get_native<backend::opencl>(Q.get_device());
   auto context_cl = get_native<backend::opencl>(Q.get_context());
   auto program_cl =
       clCreateProgramWithSource(context_cl, 1, &clSource, nullptr, &status);
-  handle(status);
-  handle(clBuildProgram(program_cl, 0, nullptr, "", nullptr, nullptr));
+  assert(CL_SUCCESS == status);
+  status = clBuildProgram(program_cl, 0, nullptr, "", nullptr, nullptr);
+  assert(CL_SUCCESS == status);
   auto kernel_cl = clCreateKernel(program_cl, "test", &status);
-  handle(status);
+  assert(CL_SUCCESS == status);
 
   return make_kernel<backend::opencl>(kernel_cl, Q.get_context());
 }
@@ -52,7 +48,7 @@ int main() {
   constexpr cl_int N_slm = 256;
   constexpr int N_wg = 32;
 
-  cl_int init[N_wg] = {-1};
+  cl_int init[N_wg];
   buffer<cl_int, 1> b(init, N_wg);
 
   Q.submit([&](handler &cgh) {
@@ -68,8 +64,8 @@ int main() {
 
   auto acc_global = b.get_access<access::mode::read>();
   for (int i = 0; i < N_wg; i++) {
-    if (acc_global[i] != 1.0f) {
-      std::cout << "Error in WG " << i << std::endl;
+    if (acc_global[i] != 1) {
+      std::cerr << "Error in WG " << i << std::endl;
       exit(1);
     }
   }
